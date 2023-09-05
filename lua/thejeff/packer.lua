7ef25e435be3a838b343f6d62215f52e90343353
J3ff5 <jeffersonsoaresbh@gmail.com>
2023-09-04 22:15:57 -0300

    :sparkles: feat: add coc and tsserver to make life better

--- lua/thejeff/packer.lua
+++ lua/thejeff/packer.lua
@@ -20,9 +20,10 @@ return require('packer').startup(function(use)
 	end
   })
 
-  use( 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use( 'nvim-treesitter/playground')
+  use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use ('nvim-treesitter/playground')
   use ('theprimeagen/harpoon')
   use ('mbbill/undotree')
-  use('tpope/vim-fugitive')
+  use ('tpope/vim-fugitive')
+  use ('neoclide/coc.nvim', {branch= 'release'})
 end)
